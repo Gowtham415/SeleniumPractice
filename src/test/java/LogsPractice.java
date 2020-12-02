7e1b24d00d1ba79711fc1161810b52d2c805408d
Gowtham Epparla <gowtham_epparla@epam.com>
2020-12-02 16:32:07 +0530

    SVG Elements and Shadow DOMS

--- src/test/java/LogsPractice.java
+++ src/test/java/LogsPractice.java
@@ -1,9 +1,9 @@
+import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.testng.annotations.BeforeMethod;
-import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -13,12 +13,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 
-public class LogsPractice {
+public class LogsPractice{
 	protected WebDriver driver;
 	protected WebDriverWait driverWait;
 	protected JavascriptExecutor jsDriver;
 	public String baseURl = "https://www.expedia.co.in/";
-	public static Logger log = Logger.getLogger(LogsPractice.class);
+	private static Logger log = LogManager.getLogger(LogsPractice.class.getName());
 
 	@BeforeMethod
 	public void beforeMethod() throws InterruptedException {
@@ -39,13 +39,15 @@ public class LogsPractice {
 		driver.findElement(
 				By.cssSelector(".uitk-flex-item.uitk-typeahead-toolbar-field.toolbar-content-v1 > div>input"))
 				.sendKeys("Hyderabad");
-		log.debug("Hello world");
+		log.debug("CSS Selector is used");
 		driver.findElement(By.xpath("//div[@id='location-field-destination-menu']//ul//li[1]/button")).click();
 		driver.findElement(By.xpath("//input[@id='d1']/following-sibling::button[1]")).click();
 		driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[contains(text(),'December')]/following::table[1]//td//button[@data-day='5']"))).click();
+		log.info("Departure Date is selected");
 		driver.findElement(By.xpath("//span[text()='Done']//ancestor::button")).click();
 		driver.findElement(By.xpath("//input[@id='d2']/following-sibling::button[1]")).click();
 		driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[contains(text(),'December')]/following::table[1]//td//button[@data-day='10']"))).click();
+		log.info("Arrival Date is selected");
 		driver.findElement(By.xpath("//span[text()='Done']//ancestor::button")).click();
 		driver.findElement(By.xpath("//button[text()='Search']")).click();
 	}
